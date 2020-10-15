5164a17119b523dc5f09cb6efe1d2a2b5dd617d9
John Pham <john@carepage.com.au>
2020-10-15 19:53:34 +1100

    created some code for audit information on aus

---
+++ WebScrapeConfig.py
@@ -0,0 +1,70 @@
+import bs4
+import requests
+import csv
+import os
+
+
+class Config:
+    def __init__(self):
+        self.baseurl = 'https://www.agedcarequality.gov.au/reports'  # https://www.agedcarequality.gov.au/reports?name=&racs_id=&suburb&state=All&prov_name=&service_type=All&previous_names=&postcode=&page=0
+        self.session = requests.session()
+        self.path = os.path.dirname(os.path.abspath(
+            __file__)) + '/homes_audit_data.csv'
+
+    def make_file(self):
+        with open(self.path, 'w', newline='') as path:
+            writer = csv.writer(path)
+            writer.writerow(
+                ['homenames', 'home_previous', 'previous', 'racsid', 'service', 'suburbs', 'states', 'post_code',
+                 'hrefdata'])
+
+    def get_audits(self):
+        page = 0
+        with open(self.path, 'a', newline='') as nfile:
+            writer = csv.writer(nfile)
+            while not page:
+                data = self.session.get(
+                    self.baseurl + '?name=&racs_id=&suburb&state=All&prov_name=&service_type=All&previous_names=&postcode=&page={}'.format(
+                        str(page)))
+                html = bs4.BeautifulSoup(data.content, 'html.parser')
+                home_name = html.find_all('h3', {'class': 'field-content'})
+                provider_name = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-prov-name'})
+                previous_names = html.find_all('div', {'class': 'views-field views-field-nothing'})
+                racs = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-racs-key'})
+                service_type = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-type'})
+                suburb = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-suburb'})
+                state = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-state'})
+                post = html.find_all('div', {'class': 'views-field views-field-field-acqsc-service-postcode'})
+                href = html.select('.field-content a')
+                hrefdata = [i['href'] for i in href]
+                homenames = [i.get_text() for i in home_name]
+                home_previous = [i.get_text().replace('\n', '').replace('Provider name: ', '') for i in provider_name]
+                previous = [i.get_text().replace('Previous service names:\n', '').replace('\n', '') for i in
+                            previous_names]
+                racsid = [i.get_text().replace('RACS/Commission ID: ', '').replace('\n', '') for i in racs]
+                service = [i.get_text().replace('Service type: ', '').replace('\n', '') for i in service_type]
+                suburbs = [i.get_text().replace('Suburb: ', '').replace('\n', '') for i in suburb]
+                states = [i.get_text().replace('State: ', '').replace('\n', '') for i in state]
+                post_code = [i.get_text().replace('Postcode: ', '').replace('\n', '') for i in post]
+                clist = [
+                    {'homenames': i, 'home_previous': j, 'previous': k, 'racsid': l, 'service': m, 'suburbs': n,
+                     'states': b,
+                     'post_code': v, 'hrefdata': u}
+                    for i, j, k, l, m, n, b, v, u in
+                    zip(homenames, home_previous, previous, racsid, service, suburbs, states, post_code, hrefdata)]
+                for each in clist:
+                    writer.writerow([each['homenames'],
+                                     each['home_previous'],
+                                     each['previous'],
+                                     each['racsid'],
+                                     each['service'],
+                                    each['suburbs'],
+                                    each['states'],
+                                    each['post_code'],
+                                    each['hrefdata']])
+                    print('writing row')
+                if len(clist) == 0:
+                    break
+                else:
+                    print(len(clist))
+                    page += 1
---
+++ main.py
@@ -0,0 +1,10 @@
+from WebScrapeConfig import Config
+
+a = Config()
+
+
+if __name__ == '__main__':
+    a.make_file()
+    a.get_audits()
+
+
